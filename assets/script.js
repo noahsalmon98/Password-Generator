7334115012e6ce557ba0a0b64010e8bf36e9c037
Noah Salmon <noahsalmon98@gmail.com>
2023-07-02 19:13:56 -0400

    added character array to js file

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,11 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add characters array
+const lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z'];
+const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const numbers = ['0','1','2','3','4','5','6','7','8','9']
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
