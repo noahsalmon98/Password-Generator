b79dc4bb2dac9388a74a1ed5e7efef7264b8bd06
Noah Salmon <noahsalmon98@gmail.com>
2023-07-04 11:25:43 -0400

    added numberOfCharacters to js

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,20 @@ const lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','
 const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const numbers = ['0','1','2','3','4','5','6','7','8','9']
 
+
+function generatePassword (){
+  let unpassword = []
+  const numberOfChar = +prompt("What is the number of character you would like to use? (Between 8 and 128 please)")
+  if (numberOfChar < 8) {
+    console.log("Too few characters. Password must be more between 8 and 128 characters long.")
+    alert("Too few characters. Password must be more between 8 and 128 characters long.")
+  } else if (numberOfChar > 128) {
+    console.log("Too many characters. Password must be more between 8 and 128 characters long.")
+    alert("Too many characters. Password must be more between 8 and 128 characters long.")
+  } else {
+    alert("Password will be "+ numberOfChar +" characters long.")
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
