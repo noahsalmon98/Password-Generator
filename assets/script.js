2cf6fc833191d63e108310a090051371a834f786
Noah Salmon <noahsalmon98@gmail.com>
2023-07-05 00:28:11 -0400

    added specail characters array

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,11 @@ var generateBtn = document.querySelector("#generate");
 const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n', 'o','p','q','r','s','t','u','v','w','x','y','z'];
 const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const numbersUsed = ['0','1','2','3','4','5','6','7','8','9'];
+const specialChar = ['!', '@', '#', '$', '%', '&', '(', ')', '*', '+','-','/',':',';','<','=','>','?','[',']','^','_','`','{','|','}','~']
 
 
 function generatePassword() {
-  console.log(lowerCase, upperCase, numbersUsed);
+  console.log(lowerCase, upperCase, numbersUsed, specialChar);
   let newpass = [];
   const characterCandidates = [];
   const numberOfChar = +prompt("What is the number of character you would like to use? (Between 8 and 128 please)");
